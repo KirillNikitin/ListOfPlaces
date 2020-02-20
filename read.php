2c735e260ea138a34f44804cbb774837165b72c2
Kirill Nikitin <kenny_nikitin@hotmail.com>
2020-02-21 00:42:40 +0200

    --db

--- read.php
+++ read.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json; charset=UTF-8");
 $obj = json_decode($_GET["x"], false);
 
-$conn = new mysqli('db4free.net:3306/sql7324007', 'sql7324007', 'ALJiM7jJBc', 'sql7324007') or die(mysqli_error($conn));
+$conn = new mysqli('db4free.net:3306', 'sql7324007', 'ALJiM7jJBc', 'sql7324007') or die(mysqli_error($conn));
 $stmt = $conn->prepare("SELECT * FROM data");
 //$stmt->bind_param("", $obj->table);
 $stmt->execute();
